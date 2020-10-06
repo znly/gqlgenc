81e8dd4b4af3e9fd515c3122c27582579704a79c
Clement Rey <cr.rey.clement@gmail.com>
2020-10-06 16:04:07 +0200

    client: casual post-tempest hacks removal

--- client/client_pool.go
+++ client/client_pool.go
@@ -1,13 +1,16 @@
 package client
 
 import (
+	"net"
 	"net/http"
+	"net/url"
 )
 
 // -----------------------------------------------------------------------------
 
 type ClientPool interface {
 	GetHost() string
+	GetEndpoint() string
 	GetClient() (*http.Client, string)
 	Refresh(reason string) error
 }
@@ -15,19 +18,30 @@ type ClientPool interface {
 // -----------------------------------------------------------------------------
 
 type defaultClientPool struct {
-	client *http.Client
-	host   string
+	client   *http.Client
+	endpoint *url.URL
+	host     string
 }
 
-func NewDefaultClientPool(host string) ClientPool {
+func NewDefaultClientPool(endpoint *url.URL) (ClientPool, error) {
+	host, _, err := net.SplitHostPort(endpoint.Host)
+	if err != nil {
+		return nil, err
+	}
+
 	return &defaultClientPool{
 		client: http.DefaultClient,
 		host:   host,
-	}
+		endpoint: endpoint,
+	}, nil
 }
 
 func (self *defaultClientPool) GetHost() string {
 	return self.host
+}
+
+func (self *defaultClientPool) GetEndpoint() string {
+	return self.endpoint.String()
 }
 
 func (self *defaultClientPool) GetClient() (*http.Client, string) {
